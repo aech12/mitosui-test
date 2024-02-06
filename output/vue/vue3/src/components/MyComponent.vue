41a07284c30abaefcc8673fa0e1524e1beb00a5f
aech12 <aech-12@hotmail.com>
2024-02-06 07:11:30 -0300

    init

---
+++ output/vue/vue3/src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{ message || "Hello" }} {{ name }}! I can run in React, Vue, Solid or
+    Svelte!
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "my-basic-component",
+
+  props: ["message"],
+
+  data() {
+    return { name: "Foo" };
+  },
+});
+</script>
