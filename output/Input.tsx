8bf3a85d74148d5595cd7e2e3ab69566d1a68ea4
Alex Howez <aech-12@hotmail.com>
2024-02-20 21:51:30 +0000

    small qol modifications

---
+++ output/Input.tsx
@@ -0,0 +1,22 @@
+"use client";
+import * as React from "react";
+import { cn } from "../lib/utils";
+
+function Input(props) {
+  const state = useLocalProxy({ inputValue: "" });
+
+  return (
+    <input
+      value={state.inputValue}
+      onChange={(event) => (state.inputValue = event.target.value)}
+      type={props.type}
+      {...props}
+      className={cn(
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        props.className
+      )}
+    />
+  );
+}
+
+export default Input;
--- src/components/Input.lite.tsx
+++ src/components/Input.lite.tsx
@@ -1,25 +1,48 @@
 import { useStore } from "@builder.io/mitosis";
-import { cn } from "../lib/utils"
+import { cn } from "../lib/utils";
 
-type Props = {
-  message: string;
-  type: string;
+interface InputProps {
+  type:
+    | "button"
+    | "checkbox"
+    | "color"
+    | "date"
+    | "datetime-local"
+    | "email"
+    | "file"
+    | "hidden"
+    | "image"
+    | "month"
+    | "number"
+    | "password"
+    | "radio"
+    | "range"
+    | "reset"
+    | "search"
+    | "submit"
+    | "tel"
+    | "text"
+    | "time"
+    | "url"
+    | "week";
   className: string;
-};
+}
 
-export default function Input(props: Props) {
+export default function Input(props: InputProps) {
   const state = useStore({
-    name: "Foo",
+    inputValue: "",
   });
 
   return (
     <input
-        type={props.type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          props.className
-        )}
-        {...props}
-      />
+      value={state.inputValue}
+      onChange={(event) => (state.inputValue = event.target.value)}
+      type={props.type}
+      className={cn(
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+        props.className
+      )}
+      {...props}
+    />
   );
 }
